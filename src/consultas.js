a60fd3894e2d8a99fa6d49af73d20ea7ab366339
Rafa Potes <potesr@uninorte.edu.co>
2026-09-03 21:04:55 -0500

    feat: implementar filter de personajes con 20 o mas episodios

--- src/consultas.js
+++ src/consultas.js
@@ -9,3 +9,11 @@ export function personajesVivosHumanos(personajes) {
     (p) => p.estado === "Alive" && p.especie === "Human"
   );
 }
+
+/**
+ * 2. filter
+ * Obtener todos los personajes que aparezcan en 20 o más episodios.
+ */
+export function personajesConMuchosEpisodios(personajes) {
+  return personajes.filter((p) => p.cantidadEpisodios >= 20);
+}
